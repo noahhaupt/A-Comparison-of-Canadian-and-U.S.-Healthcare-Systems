66a47919018ab2a812c3bf6942862f56e9d50b7f
Noah Haupt <noahhaupt@noahs-air-62.home>
2025-10-17 00:50:57 -0400

    Created the code for plot 1
    Plot 1 describes life expectancy in Canada vs USA every 5 years from 1990 to 2020. The data had to be filtered even more to be every 5 years and also Canada and USA split up. The reason it is every 5 years is because every year would create a very messy graph that would not give any information to the reader.

--- Plot1.py
+++ Plot1.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Filtered data (from previous python sheet):
 import pandas as pd
+df = pd.read_csv("data/healthcare_data.csv")
+specific_countries = ["Canada","USA"]
+filtered_data = df[df["Country"].isin(specific_countries)].filter(items=["Year","Country","Life_Expectancy","Doctors_per_1000","Healthcare_Spending_GDP_percent","Infant_Mortality_per_1000"])
 
-data = pd.read_csv("data/healthcare_data.csv")
+# Life expectancy vs time between us and canada every 5 years between 1990 and 2020
+
+# We need to filter the data even more to contain every 5 years between 1990 and 2020 in order to simplify the graph.
+
+specific_years = [1990, 1995, 2000, 2005, 2010, 2015, 2020]
+
+filtered_data_years = df[(df["Year"].isin(specific_years)) & (df["Country"].isin(specific_countries))].filter(items=["Year","Country","Life_Expectancy","Doctors_per_1000","Healthcare_Spending_GDP_percent","Infant_Mortality_per_1000"])
+
+# Now let's split up the data into USA and Canada
+
+canada_data = df[(df["Year"].isin(specific_years)) & (df["Country"].isin(["Canada"]))].filter(items=["Year","Country","Life_Expectancy","Doctors_per_1000","Healthcare_Spending_GDP_percent","Infant_Mortality_per_1000"])
+    
+us_data = df[(df["Year"].isin(specific_years)) & (df["Country"].isin(["USA"]))].filter(items=["Year","Country","Life_Expectancy","Doctors_per_1000","Healthcare_Spending_GDP_percent","Infant_Mortality_per_1000"])
+
+# Plotting the data
 
 import matplotlib.pyplot as plt
 
-canada = data[data["Country"] == "Canada"].sort_values("Year")
-usa = data[data["Country"] == "USA"].sort_values("Year")
-
-plt.plot(canada["Year"], canada["Life_Expectancy"], label="Canada", linestyle="solid")
-plt.plot(usa["Year"], usa["Life_Expectancy"], label="USA", linestyle="dashed")
-
-plt.title("Life Expectancy in Canada and USA (1990–2020)")
+plt.plot(canada_data.Year,canada_data.Life_Expectancy, label = "Canada", marker = "o")
+plt.plot(us_data.Year,us_data.Life_Expectancy, label = "USA", marker = "o")
+plt.title("LLife Expectancy in Canada vs USA (Every 5 Years from 1990 to 2020)")
 plt.xlabel("Year")
 plt.ylabel("Life Expectancy (years)")
 plt.legend()
-plt.grid()
+plt.grid(True)
 plt.show()
