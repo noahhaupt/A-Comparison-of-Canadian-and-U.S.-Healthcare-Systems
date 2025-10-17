a9159ddfed9d144b14c096f7b1475e713142ab9c
Noah Haupt <noahhaupt@noahs-air-62.home>
2025-10-17 00:51:55 -0400

    fixed small mistake

--- Plot1.py
+++ Plot1.py
@@ -7,7 +7,6 @@ df = pd.read_csv("data/healthcare_data.csv")
 specific_countries = ["Canada","USA"]
 filtered_data = df[df["Country"].isin(specific_countries)].filter(items=["Year","Country","Life_Expectancy","Doctors_per_1000","Healthcare_Spending_GDP_percent","Infant_Mortality_per_1000"])
 
-# Life expectancy vs time between us and canada every 5 years between 1990 and 2020
 
 # We need to filter the data even more to contain every 5 years between 1990 and 2020 in order to simplify the graph.
 
@@ -27,7 +26,7 @@ import matplotlib.pyplot as plt
 
 plt.plot(canada_data.Year,canada_data.Life_Expectancy, label = "Canada", marker = "o")
 plt.plot(us_data.Year,us_data.Life_Expectancy, label = "USA", marker = "o")
-plt.title("LLife Expectancy in Canada vs USA (Every 5 Years from 1990 to 2020)")
+plt.title("Life Expectancy in Canada vs USA (Every 5 Years from 1990 to 2020)")
 plt.xlabel("Year")
 plt.ylabel("Life Expectancy (years)")
 plt.legend()
